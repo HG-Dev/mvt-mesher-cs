87f00abc699975201bef2a385f95c6bbf8cd18ae
Ian M <ian.hg.dev@gmail.com>
2025-12-03 14:39:48 +0900

    Explanations for compiler services entries

--- MvtMesherCore/_CompilerServices.cs
+++ MvtMesherCore/_CompilerServices.cs
@@ -1,9 +1,15 @@
 namespace System.Runtime.CompilerServices;
 
+/// <summary>
+/// Helper class to enable 'init' accessors in C# 9+
+/// </summary>
 public class IsExternalInit
 {
 }
 
+/// <summary>
+/// Indicates that a member is required for object initialization.
+/// </summary>
 public class RequiredMemberAttribute
 {
 }
